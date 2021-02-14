ca944f8c6c3b2d729dab735c4fdeff3b39d6cda3
DVC Tester <dvctester@example.com>
2021-02-14 12:18:29 +0000

    date

--- site-content/src/scripts/index.js
+++ site-content/src/scripts/index.js
@@ -1,4 +1,9 @@
 //import './modules/list';
-import '../styles/index.scss';
-
-import './modules/main';
+import "../styles/index.scss";
+import "bootstrap";
+import "jquery";
+import "jquery-ui";
+import "jquery-sticky";
+import AOS from "aos";
+import "owl.carousel";
+import "landingpage.js";
